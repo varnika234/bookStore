f9417b15b945fb01f61e899a7ab878c07e3e72fb
varnika234 <121311204+varnika234@users.noreply.github.com>
2024-08-09 23:59:02 +0530

    Update Course.jsx

--- Frontend/Frontend/src/components/Course.jsx
+++ Frontend/Frontend/src/components/Course.jsx
@@ -1,52 +1,34 @@
-import React, { useEffect, useState } from "react";
-import Cards from "./Cards";
-import axios from "axios";
-import { Link } from "react-router-dom";
+import React from 'react'
+import  list from "../../public/list.json"
+import Cards from './Cards'
+import { Link } from 'react-router-dom'
+
 function Course() {
-  const [book, setBook] = useState([]);
-  useEffect(() => {
-    const getBook = async () => {
-      try {
-        const res = await axios.get("http://localhost:4001/book");
-        console.log(res.data);
-        setBook(res.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    getBook();
-  }, []);
   return (
     <>
-      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
-        <div className="mt-28 items-center justify-center text-center">
-          <h1 className="text-2xl  md:text-4xl">
-            We're delighted to have you{" "}
-            <span className="text-pink-500"> Here! :)</span>
-          </h1>
-          <p className="mt-12">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro,
-            assumenda? Repellendus, iste corrupti? Tempore laudantium
-            repellendus accusamus accusantium sed architecto odio, nisi expedita
-            quas quidem nesciunt debitis dolore non aspernatur praesentium
-            assumenda sint quibusdam, perspiciatis, explicabo sequi fugiat amet
-            animi eos aut. Nobis quisquam reiciendis sunt quis sed magnam
-            consequatur!
-          </p>
-          <Link to="/">
-            <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
-              Back
-            </button>
-          </Link>
+    <div className='max-w-screen-2xl container mx-auto md:px-20 px-4 dark:bg-gray-900 dark:text-white '>
+        <div className='mt-28 items-center justify-center text-center'>
+            <h1 className='text-2xl md:text-4xl'>We are Delight to have you {" "}<span className='text-pink-500'>Here! :)</span> </h1>
+            <p className='mt-12'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam voluptatem cumque ipsa facilis nisi odit natus dolorem consequuntur possimus quaerat eius voluptas sequi, blanditiis obcaecati quod incidunt exercitationem laboriosam Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, necessitatibus unde? Unde accusamus eius libero. Veritatis maiores molestiae labore minus.
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat ea dolore cum laborum beatae excepturi laudantium commodi culpa maiores corrupti!
+            </p>
+            <Link to="/">
+            <button className=' mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300 '>Back</button>
+            </Link>
         </div>
-        <div className="mt-12 grid grid-cols-1 md:grid-cols-4">
-          {book.map((item) => (
-            <Cards key={item.id} item={item} />
-          ))}
+        <div className='mt-12 grid grid-cols-1 md:grid-cols-4'>
+            {
+               list.map((item)=>(
+                <Cards key={item.id} item={item}/>
+               ))
+            }
         </div>
-      </div>
+      
+    </div>
     </>
-  );
+  )
 }
 
-export default Course;
+export default Course
+
